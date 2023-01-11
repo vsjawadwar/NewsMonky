7f6dfebd9137073ce5e4a7cad260d80bebc4e60d
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-01-11 20:51:38 +0530

    State and props defined and implemented as per SampleOutput JSON later on I'll fetch this data from NewsAPI

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description}=this.props;
+    let { title, description, imgUrl } = this.props;
     return (
       <div className='container my-3'>
-        <div className="card" style={{width: "18rem"}}>
-  <img src="https://www.reuters.com/resizer/CLRzQjonglzi2uDp26ehPmz1jNQ=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/SGGJUH3QS5K33PSX4IGTUAQPYM.jpg" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/abcd" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/abcd" className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
